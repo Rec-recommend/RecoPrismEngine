e2fc86df44adea697713cbb2c60bbb1f66bece62
AbanoubMagdyAdly <abanoub.magdy.adly@gmail.com>
2019-05-27 15:03:53 +0200

    Update tenantdb handler to use query builder

--- DbHandler/TenantDbHandler.py
+++ DbHandler/TenantDbHandler.py
@@ -1,32 +1,52 @@
 #!/usr/bin/env python3
 
 from DbHandler.DbHandler import DbHandler
+from DbHandler.EAVQueryBuilder import EAVQueryBuilder
+
 
 class TenantDbHandler:
-    def __init__(self , DB_name):
+
+    def __init__(self, db_name):
         self.tenant_DbHandler = DbHandler()
-        self.DB_name = DB_name
-        self.db = self.tenant_DbHandler.connect_to_db(self.DB_name)
+        self.db_name = db_name
+        self.db = self.tenant_DbHandler.connect_to_db(self.db_name)
 
-    def get_tables(self):
+    def get_eav_attributes(self, column,entity_name):
+        q = "SELECT " + column + " FROM attributes \
+        where entity_id = (select id from entities where name ='" + entity_name + "')"
         cursor = self.db.cursor()
-        cursor.execute("SELECT name FROM configs where target = 'table'")
-        return [item[0] for item in cursor.fetchall()]
+        cursor.execute(q)
+        weights = cursor.fetchall()
+        x = []
+        for item in weights:
+            x.extend(item)
+        return x
 
-    def get_columns(self,table_name):
+    def get_eav_table(self, entity_name):
+        labels = self.get_eav_attributes("label", entity_name)
+        weights = self.get_eav_attributes("weight", entity_name)
+        qb = EAVQueryBuilder(entity_name)
+        for label in labels:
+            qb.attribute(label)
+        q = qb.build()
         cursor = self.db.cursor()
-        cursor.execute(f"SELECT name , weight  FROM configs where target = 'column' and  `table` = '{table_name}' and used = 1")
-        return cursor.fetchall()
+        print(q)
+        cursor.execute(q)
 
-    def get_table_data(self , table_name):
+        data = cursor.fetchall()
+        labels.insert(0, 'id')
+        weights.insert(0, 0)
+        return {
+            "column_names": labels,
+            "weights": weights,
+            "data": data
+        }
+
+    def get_pivot_table(self, pivot_table):
         cursor = self.db.cursor()
-        cursor.execute(f"SELECT * FROM {table_name}")
+        cursor.execute('select user_id, item_id, value from ' + pivot_table)
+        data = cursor.fetchall()
         return {
             "column_names": cursor.column_names,
-            "data": cursor.fetchall()
-            }
-
-
-
-# DbHandler = TenantDbHandler('cart-on-rails')
-# DbHandler.get_table_data('users')
+            "data": data
+        }
