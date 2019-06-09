9db3bd8951c2154f64d39f002d019e65dfbf4417
Hussein-ElAttar <hussien.elattar1@gmail.com>
2019-06-09 03:07:50 +0200

    Store recommendations in mongodb

--- main.py
+++ main.py
@@ -3,11 +3,13 @@
 from DbHandler.TenantDbHandler import TenantDbHandler
 from Recommenders.CollborativeRecommender import CollborativeRecommender
 import pandas as pd
+from pymongo import MongoClient
 
-# Helper functions #######################################
+tenant_db_name = 'tn_GfxfFrKRovHKGJ4PNr4lOArxQNQEb'
 
-# dict for movies names and ids
-# --------------------------------
+# =====================================================
+# Helper functions: dict for movies names and ids
+# -----------------------------------------------------
 movies_df = pd.read_csv('./Recommenders/movies.csv')
 movieID_to_name = {}
 
@@ -17,41 +19,47 @@ for index, row in movies_df.iterrows():
 	movieID_to_name[movieID] = movieName
 
 
+# =====================================================
 # store table in a dataframe
-# --------------------------------
+# -----------------------------------------------------
 def prepare_df(table):
     df = pd.DataFrame(table['data'])
     df.columns = table['column_names']
     return df
 
+
 # =====================================================
-
-
-tenant = TenantDbHandler('tn_GfxfFrKRovHKGJ4PNr4lOArxQNQEb')
+# load data from mysql and train the model
+# -----------------------------------------------------
+tenant = TenantDbHandler(tenant_db_name)
 items_table = tenant.get_pivot_table('ratings')
 df = prepare_df(items_table)
-print(df.head(20))
 
 
 line_format = 'user item rating'
-sim_options = {'name': 'pearson_baseline', 'user_based': True, 'min_support': 15}
+sim_options = {'name': 'pearson_baseline', 'user_based': True, 'min_support': 7}
 
 cfr = CollborativeRecommender(sim_options, df, line_format)
 
 
-movies = ['115617', '2424', '54001', '88163']
+# =====================================================
+# get users recommendations and store them in mongodb
+# -----------------------------------------------------
+users = df['end_user_id'].unique()
+mongo_client = MongoClient()
+mongo_db = mongo_client[tenant_db_name]
+count = 0
+recos = []
+for user in users:
+    res = cfr.get_recommendations(user)
+    recos.append({'user_id':int(user), 'recommendations':[i[0] for i in res]})
+    count += 1
+    if count > 100000:
+        mongo_db.recommendations.insert_many(recos)
+        count = 0
+        recos = []
 
-for movie in movies:
-	res = cfr.get_recommendations(movie)
-	for movie in res:
-		print(movieID_to_name[movie[0]] , " ---- " , movie[1])
-	print("--------------------------------------")
-
-
-
-
-
-
+# # =====================================================
 
 
 
@@ -60,3 +68,17 @@ for movie in movies:
 # print(t.get_eav_table("items"))
 # file_path = "./Recommenders/ratings.1.csv"
 # cfr = CollborativeRecommender(sim_options, file_path, line_format)
+
+# =====================================================
+# SELECT value FROM `iav` WHERE attribute_id = 1 AND item_id in (SELECT `item_id` FROM `ratings` WHERE end_user_id = 360 AND value = 5);
+# SELECT value FROM `iav` WHERE item_id = 8864 AND attribute_id = 1
+
+
+# users = ['611','612', '613']
+
+# for user in users:
+# 	res = cfr.get_recommendations(user)
+# 	for movie in res:
+# 		print(movieID_to_name[movie[0]] , " ---- " , movie[1])
+# 	print("--------------------------------------")
+
