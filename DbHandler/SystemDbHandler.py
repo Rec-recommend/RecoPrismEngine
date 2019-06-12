4d359530e14f16984eff16c75ba42e4ce5780473
Hussein-ElAttar <hussien.elattar1@gmail.com>
2019-06-13 00:48:01 +0200

    Rename read_tenant_db_names

--- DbHandler/SystemDbHandler.py
+++ DbHandler/SystemDbHandler.py
@@ -7,7 +7,7 @@ class SystemDbHandler:
         self.system_DbHandler = DbHandler()
         self.db = self.system_DbHandler.connect_to_db("recoprism")
 
-    def read_tenant_db_names(self):
+    def get_tenant_db_names(self):
         cursor = self.db.cursor()
         cursor.execute("SELECT uuid FROM websites")
         return [item[0] for item in cursor.fetchall()]
